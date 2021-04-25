5518daab214e2ee172bf40167e61f0aea917d86a
Nitin v <nitvishnoi@gmail.com>
2021-04-25 19:33:35 +0000

    Particle filter files added

--- src/particle_filter.h
+++ src/particle_filter.h
@@ -63,8 +63,11 @@ class ParticleFilter {
    * @param predicted Vector of predicted landmark observations
    * @param observations Vector of landmark observations
    */
+  //void dataAssociation(std::vector<LandmarkObs> predicted, 
+    //                   std::vector<LandmarkObs>& observations);
+
   void dataAssociation(std::vector<LandmarkObs> predicted, 
-                       std::vector<LandmarkObs>& observations);
+                       std::vector<LandmarkObs>& observations,double sensor_range);
   
   /**
    * updateWeights Updates the weights for each particle based on the likelihood
